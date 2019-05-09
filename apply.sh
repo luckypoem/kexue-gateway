baae54df8b040125163fc41c2fdc1fcc51accd2e
wi1dcard <wi1dcard.cn@gmail.com>
2019-05-10 00:41:22 +0800

    The apply.sh now accept more options which will be passing through to Ansible.

--- apply.sh
+++ apply.sh
@@ -8,17 +8,28 @@ fi
 BASENAME=$(basename $0)
 INVENTORY=inventory
 
-if [[ $1 == "" ]]; then
-    echo "Usage: $BASENAME <variable_file>"
+if [[ "$1" == "" ]]; then
+    echo "Usage: $BASENAME <variable_file> [-|<options_to_ansible>]"
     exit 2
 fi
 
-if [ ! -f "$INVENTORY" ]; then
-    while [ "$IP" == "" ]; do
+if [[ ! -f "$INVENTORY" ]]; then
+    while [[ "$IP" == "" ]]; do
         printf "Please input your NanoPi IP: "
         read IP
     done;
     echo "$IP" > "$INVENTORY"
 fi
 
-ansible-playbook -vvv --ask-pass -i "$INVENTORY" -e "@$1" playbook.yml
+EXTRA_VARS=$1
+shift
+
+if [[ "$@" == "" ]]; then
+    MORE_OPTIONS="-vvv --ask-pass"
+elif [[ "$1" == "-" ]]; then
+    MORE_OPTIONS=""
+else
+    MORE_OPTIONS="$@"
+fi
+
+ansible-playbook $MORE_OPTIONS -i "$INVENTORY" -e "@$EXTRA_VARS" playbook.yml
