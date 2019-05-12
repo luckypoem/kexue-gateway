3c2192106491ad9bfe431dcb06f4f245c9ea4f11
wi1dcard <wi1dcard.cn@gmail.com>
2019-05-12 20:42:58 +0800

    Make sure GIt LFS installed before applying.

--- apply.sh
+++ apply.sh
@@ -1,35 +1,47 @@
 #!/usr/bin/env bash
 
-if ! command -v ansible-playbook >/dev/null 2>&1; then
-    echo "Please make sure Ansible is installed on your local machine."
-    exit 1
-fi
+ensure_dep() {
+    if ! command -v "$1" &>/dev/null; then
+        echo "Please make sure $2 is installed on your local machine."
+        exit 1
+    fi
+}
 
-BASENAME=$(basename $0)
-INVENTORY=inventory
+preflight() {
+    ensure_dep "ansible-playbook" "Ansible"
+    ensure_dep "git-lfs" "Git LFS"
+}
 
-if [[ "$1" == "" ]]; then
-    echo "Usage: $BASENAME <variable_file> [-|<options_to_ansible>]"
-    exit 2
-fi
+main() {
+    BASENAME=$(basename $0)
+    INVENTORY=inventory
 
-if [[ ! -f "$INVENTORY" ]]; then
-    while [[ "$IP" == "" ]]; do
-        printf "Please input your NanoPi IP: "
-        read IP
-    done;
-    echo "$IP" > "$INVENTORY"
-fi
+    if [[ "$1" == "" ]]; then
+        echo "Usage: $BASENAME <variable_file> [-|<options_to_ansible>]"
+        exit 2
+    fi
 
-EXTRA_VARS=$1
-shift
+    if [[ ! -f "$INVENTORY" ]]; then
+        while [[ "$IP" == "" ]]; do
+            printf "Please input your NanoPi IP: "
+            read IP
+        done;
+        echo "$IP" > "$INVENTORY"
+    fi
 
-if [[ "$@" == "" ]]; then
-    MORE_OPTIONS="-vvv --ask-pass"
-elif [[ "$1" == "-" ]]; then
-    MORE_OPTIONS=""
-else
-    MORE_OPTIONS="$@"
-fi
+    EXTRA_VARS=$1
+    shift
 
-ansible-playbook $MORE_OPTIONS -i "$INVENTORY" -e "@$EXTRA_VARS" playbook.yml
+    if [[ "$@" == "" ]]; then
+        MORE_OPTIONS="-vvv --ask-pass"
+    elif [[ "$1" == "-" ]]; then
+        MORE_OPTIONS=""
+    else
+        MORE_OPTIONS="$@"
+    fi
+
+    ansible-playbook $MORE_OPTIONS -i "$INVENTORY" -e "@$EXTRA_VARS" playbook.yml
+}
+
+preflight
+main $@
